cc161380a9017f6d691276afb93f7c028a4ddb2b
promeos <christopher.logan.ortiz@gmail.com>
2021-05-22 21:21:16 -0500

    Change project

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .acquire import get_financial_data
-from .prepare import *
-from .env import config
--- src/sql_db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-import env
-
-
-def get_connection(db, user=env.user, host=env.host, password=env.password):
-    '''
-    Returns a formatted url to access a SQL database.
-    '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'

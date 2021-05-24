ebda63b5bd648a2a796f4422ca8792bde9bff118
promeos <christopher.logan.ortiz@gmail.com>
2021-05-24 00:20:43 -0500

    Acquire function for second dataset completed

--- src/acquire.py
+++ src/acquire.py
@@ -2,18 +2,37 @@ import pandas as pd
 import numpy as np
 import pymysql
 
-####################################### Acquire World Bank Data ################################
 
+####################################### Acquire World Bank Data ################################
 def get_contract_data():
     '''
-    Returns a dataframe of World Bank project contracts.
+    Returns a dataframe of World Bank contracts.
     '''
     data = pd.read_csv('.\data\wb_contracts.csv')
     return data
 
+
+def get_project_data():
+    '''
+    Returns a dataframe of World Bank project.
+    '''
+    data = pd.read_excel('.\data\wb_projects.xlsx', engine='openpyxl', skiprows=[0, 2])
+    return data
+
+
 ####################################### MySQL Database #########################################
 def get_connection(user='', password='', host='localhost', port='3306', database='worldbank'):
     '''
-    Returns a formatted url to access a local SQL Database.
+    Returns a formatted url to access a local SQL Database compatible with Pandas `to_sql()` function.
     '''
     return f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
+
+
+def get_cursor(user='', password=''):
+    '''
+    Creates a connection to a MySQL database and returns the connection and a Cursor Object to interact with the database.
+    '''
+    cnx = pymysql.connect(user=user, password=password, host='localhost', port=3306)
+    cursor = cnx.cursor()
+
+    return cnx, cursor
